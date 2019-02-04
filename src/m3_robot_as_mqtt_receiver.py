a08ff1d3772543ee5cfc0e78fdecdf2de79f6d4a
hedricsd <hedricsd@rose-hulman.edu>
2019-02-04 14:59:26 -0500

    Done

--- src/m3_robot_as_mqtt_receiver.py
+++ src/m3_robot_as_mqtt_receiver.py
@@ -5,3 +5,29 @@ Using a Brickman (robot) as the receiver of messages.
 # Same as m2_fake_robot_as_mqtt_sender,
 # but have the robot really do the action.
 # Implement just FORWARD at speeds X and Y is enough.
+
+import m2_tkinter_as_mqtt_sender as com
+import time
+
+
+class DelegateThatReceives(object):
+
+    def say_it(self, message):
+        print("Message received!", message)
+
+
+def main():
+    name1 = input("Enter one name (subscriber): ")
+    name2 = input("Enter another name (publisher): ")
+
+    my_delegate = DelegateThatReceives()
+    mqtt_client = com.MqttClient(my_delegate)
+    mqtt_client.connect(name1, name2)
+    time.sleep(1)  # Time to allow the MQTT setup.
+    print()
+
+    while True:
+        time.sleep(0.01)  # Time to allow message processing
+
+
+main()
